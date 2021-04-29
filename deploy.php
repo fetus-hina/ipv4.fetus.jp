203da226cf1adf5245a32ee40b19ad49c5fd86c6
AIZAWA Hina <hina@fetus.jp>
2021-04-29 22:12:18 +0900

    deployer

---
+++ deploy.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deployer;
+
+require('recipe/yii2-app-basic.php');
+
+set('application', 'ipv4.fetus.jp');
+set('repository', 'git@github.com:fetus-hina/ipv4.fetus.jp.git');
+set('composer_options', implode(' ', [
+    'install',
+    '--verbose',
+    '--prefer-dist',
+    '--no-progress',
+    '--no-interaction',
+    '--optimize-autoloader',
+    '--no-suggest',
+]));
+set('git_tty', true);
+add('shared_files', [
+    'config/components/db/password.php',
+    'config/components/web/request--cookie.php',
+    'config/params/adsense.php',
+]);
+add('shared_dirs', [
+    'runtime',
+]);
+add('writable_dirs', [
+    'runtime',
+    'web/assets',
+]);
+set('writable_mode', 'chmod');
+set('writable_chmod_recursive', false);
+set('softwarecollections', []);
+
+set('bin/php', function () {
+    if ($scl = get('softwarecollections')) {
+        return vsprintf('scl enable %s -- php', [
+            implode(' ', array_map(
+                'escapeshellarg',
+                $scl
+            )),
+        ]);
+    }
+
+    return locateBinaryPath('php');
+});
+
+set('bin/npm', function () {
+    if ($scl = get('softwarecollections')) {
+        return vsprintf('scl enable %s -- npm', [
+            implode(' ', array_map(
+                'escapeshellarg',
+                $scl
+            )),
+        ]);
+    }
+
+    return locateBinaryPath('npm');
+});
+
+host('ayanami.single-quote.com')
+    ->user('ipv4')
+    ->stage('production')
+    ->roles('app')
+    ->set('deploy_path', '~/ipv4appv2')
+    ->set('softwarecollections', [
+        'php74',
+        'rh-nodejs14',
+    ]);
+
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:git_config',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:production',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'deploy:run_migrations',
+    'deploy:build',
+    'deploy:symlink',
+    'deploy:clear_opcache',
+    'deploy:clear_proxy',
+    'deploy:unlock',
+    'cleanup',
+])->desc('Deploy the project');
+
+task('deploy:git_config', function () {
+    run('git config --global advice.detachedHead false');
+});
+
+task('deploy:production', function () {
+    within('{{release_path}}', function () {
+        run('touch .production');
+        run('rm -f web/index.test.php');
+    });
+});
+
+task('deploy:vendors', function () {
+    within('{{release_path}}', function () {
+        run('{{bin/composer}} {{composer_options}}');
+        run('{{bin/npm}} clean-install');
+    });
+});
+
+task('deploy:build', function () {
+    within('{{release_path}}', function () {
+        if ($scl = get('softwarecollections')) {
+            run(vsprintf('scl enable %s -- make', [
+                implode(' ', array_map(
+                    'escapeshellarg',
+                    $scl
+                )),
+            ]));
+        } else {
+            run('make');
+        }
+    });
+});
+
+task('deploy:clear_opcache', function () {
+    // run('curl -f --insecure --resolve ipv4.fetus.jp:443:127.0.0.1 https://ipv4.fetus.jp/site/clear-opcache');
+});
+
+task('deploy:clear_proxy', function () {
+});
+
+after('deploy:failed', 'deploy:unlock');
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ue
+
+BASE_VERSION="2.0"
+
+GIT_COMMIT_DATE=$(git log --date=iso --pretty=format:"%cd" -n 1)
+VERSION_DATE=$(date --utc --date="$GIT_COMMIT_DATE" '+%Y%m%d.%H%M%S')
+VERSION_TAG="v${BASE_VERSION}.${VERSION_DATE}"
+
+git tag -m $VERSION_TAG $VERSION_TAG || /bin/true
+git push origin master $VERSION_TAG
+
+./vendor/bin/dep deploy --tag=$VERSION_TAG production
