641b303efe3ef5e56aac4e37e27919815c2be2dc
AIZAWA Hina <hina@fetus.jp>
2021-04-29 22:11:06 +0900

    Update DB config

--- config/components/db/db.php
+++ config/components/db/db.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 use yii\db\Connection;
 
-return [
-    'class' => Connection::class,
-    'dsn' => 'pgsql:host=localhost;dbname=ipv4',
-    'username' => 'ipv4',
-    'password' => file_exists(__DIR__ . '/password.php')
-        ? require(__DIR__ . '/password.php')
-        : 'ipv4',
-    'charset' => 'utf8',
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+return (function (): array {
+    $config = [
+        'class' => Connection::class,
+        'dsn' => 'pgsql:host=localhost;dbname=ipv4app2',
+        'username' => 'ipv4app2',
+        'password' => file_exists(__DIR__ . '/password.php')
+            ? require(__DIR__ . '/password.php')
+            : 'ipv4app2',
+        'charset' => 'utf8',
+    ];
+
+    if (YII_ENV_PROD) {
+        $config = array_merge($config, [
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 60,
+            'schemaCache' => 'cache',
+        ]);
+    }
+
+    return $config;
+})();
