02e96d4ab37b92db6dee68587ceac9cb3c05086d
AIZAWA Hina <hina@fetus.jp>
2021-04-30 15:21:22 +0900

    assetのパス計算時にリビジョンを見る

--- config/components/web/asset-manager.php
+++ config/components/web/asset-manager.php
@@ -45,4 +45,17 @@ return [
             'sourcePath' => '@npm/yii2-pjax',
         ],
     ],
+    'hashCallback' => function (string $path): string {
+        $gitRevision = Yii::$app->params['gitRevision'];
+        $path = is_file($path) ? dirname($path) : $path;
+        if (!$gitRevision) {
+            return substr(hash('sha256', $path, 0, 16));
+        }
+
+        if ($gitRevision['version']) {
+            return $gitRevision['version'] . '/' . substr(hash('sha256', $path), 0, 16);
+        }
+
+        return 'rev-' . $gitRevision['short'] . '/' . substr(hash('sha256', $path), 0, 16);
+    },
 ];
