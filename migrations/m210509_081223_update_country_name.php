a36b72eef9f1c95464382d01bb63a0107be4e013
AIZAWA Hina <hina@fetus.jp>
2021-05-09 17:28:55 +0900

    Update some name of countries/regions

---
+++ migrations/m210509_081223_update_country_name.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+use yii\db\Migration;
+
+final class m210509_081223_update_country_name extends Migration
+{
+    public function safeUp()
+    {
+        return $this->doUpdate('new');
+    }
+
+    public function safeDown()
+    {
+        return $this->doUpdate('old');
+    }
+
+    private function doUpdate(string $key): bool
+    {
+        foreach ($this->getData() as $cc => $info) {
+            $this->update(
+                '{{%region}}',
+                [
+                    'name_ja' => $info[$key]['ja'],
+                    'name_en' => $info[$key]['en'],
+                ],
+                [
+                    'id' => $cc,
+                ]
+            );
+        }
+
+        return true;
+    }
+
+    private function getData(): array
+    {
+        return [
+            'mk' => [
+                'old' => [
+                    'ja' => 'マケドニア旧ユーゴスラビア共和国',
+                    'en' => 'Macedonia, the former Yugoslav Republic of',
+                ],
+                'new' => [
+                    'ja' => '北マケドニア共和国',
+                    'en' => 'North Macedonia, Republic of',
+                ],
+            ],
+            'ps' => [
+                'old' => [
+                    'ja' => 'パレスチナ',
+                    'en' => 'Palestinian Territory, Occupied',
+                ],
+                'new' => [
+                    'ja' => 'パレスチナ',
+                    'en' => 'Palestine',
+                ],
+            ],
+            'tz' => [
+                'old' => [
+                    'ja' => 'タンザニア',
+                    'en' => 'Tanzania, United Republic of',
+                ],
+                'new' => [
+                    'ja' => 'タンザニア連合共和国',
+                    'en' => 'Tanzania, United Republic of',
+                ],
+            ],
+        ];
+    }
+}
