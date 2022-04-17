146a74fab081a0b3e74f64a5b970f24742da5246
AIZAWA Hina <hina@fetus.jp>
2022-04-17 12:22:50 +0900

    v2.12

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -ue
 
-BASE_VERSION="2.11"
+BASE_VERSION="2.12"
 
 GIT_COMMIT_DATE=$(git log --date=iso --pretty=format:"%cd" -n 1)
 VERSION_DATE=$(date --utc --date="$GIT_COMMIT_DATE" '+%Y%m%d.%H%M%S')
