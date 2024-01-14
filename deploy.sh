8f381beb018a1755f26e4822732879c946e2214e
AIZAWA Hina <hina@fetus.jp>
2024-01-14 22:54:05 +0900

    Requires PHP 8.2

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -ue
 
-BASE_VERSION="4.0"
+BASE_VERSION="5.0"
 
 GIT_COMMIT_DATE=$(git log --date=iso --pretty=format:"%cd" -n 1)
 VERSION_DATE=$(date --utc --date="$GIT_COMMIT_DATE" '+%Y%m%d.%H%M%S')
